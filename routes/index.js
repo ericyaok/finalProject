9784a8089d0307eff372ed2ee24b8db38715493b
Eric Kodzi <kod24002@byui.edu>
2025-04-17 09:21:45 +0000

    update index routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const usersRouter = require('./users');
 const booksRouter = require('./books');
+const reviewsRouter = require('./reviews');
+const borrowsRouter = require('./borrows');
 
 router.get('/', (req, res) => {
     res.send('Welcome To The Library Management System');
@@ -10,7 +12,7 @@ router.get('/', (req, res) => {
 
 router.use('/users', usersRouter);
 router.use('/books', booksRouter);
-router.use('/reviews', booksRouter);
-router.use('/borrows', booksRouter);
+router.use('/reviews', reviewsRouter);
+router.use('/borrows', borrowsRouter);
 
 module.exports = router;
