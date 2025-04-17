b7f138fc47668507233127361918ab4f93f6c8a5
Eric Kodzi <kod24002@byui.edu>
2025-04-17 09:51:08 +0000

    add githun oauth

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,20 @@ const booksRouter = require('./books');
 const reviewsRouter = require('./reviews');
 const borrowsRouter = require('./borrows');
 
+/* router.get('/', (req, res) => {
+  res.send('Welcome To The Library Management System');
+});
+ */
+
 router.get('/', (req, res) => {
-    res.send('Welcome To The Library Management System');
-  }); 
+  if (req.isAuthenticated()) {
+    // User is logged in
+    res.send(`Logged in as ${req.user.username} Welcome To The Library Management System`);  // Or use req.user.displayName or any other GitHub profile field
+  } else {
+    // User is not logged in
+    res.send('Logged Out');
+  }
+});
 
 router.use('/users', usersRouter);
 router.use('/books', booksRouter);
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ dotenv.config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger-output.json');
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 
 
@@ -19,6 +22,66 @@ app.use('/', require('./routes'));
 
 app.use(cors()); // Enable CORS for all routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
+
+// Serialize user to session
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, done) {
+        // You can create or find the user in your DB here
+        return done(null, profile);
+    }
+));
+
+
+app.use(session({
+    secret: 'your-session-secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Trigger GitHub login
+app.get('/auth/github',
+    //passport.authenticate('github', { scope: ['user:email'] })
+    passport.authenticate('github'), (req, res) => { }
+);
+
+// GitHub callback
+app.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/auth/github' }),
+    function (req, res) {
+        // Successful login
+        res.redirect('/');
+    }
+);
+
+// Logout route
+app.get('/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/');
+    });
+});
+
+
+
+
+
 
 
 mongodb.initDb((err) => {
